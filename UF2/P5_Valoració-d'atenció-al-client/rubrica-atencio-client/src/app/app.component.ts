da619425a99823076740d02794126983ca5e6155
XavierCanal <xcanal8@boscdelacoma.cat>
2022-12-14 18:38:56 +0100

    Initial Structure

---
+++ UF2/P5_Valoració-d'atenció-al-client/rubrica-atencio-client/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular';
+
+  productForm: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.productForm = this.fb.group({
+      name: '',
+      quantities: this.fb.array([]) ,
+    });
+  }
+
+  quantities() : FormArray {
+    return this.productForm.get("quantities") as FormArray
+  }
+
+  newQuantity(): FormGroup {
+    return this.fb.group({
+      qty: '',
+      price: '',
+    })
+  }
+
+  addQuantity() {
+    this.quantities().push(this.newQuantity());
+  }
+
+  removeQuantity(i:number) {
+    this.quantities().removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
+}
---
+++ UF2/P5_Valoració-d'atenció-al-client/rubrica-atencio-client/src/app/project/Models/Entities/Interfaces/IValoration/ivaloration.ts
@@ -0,0 +1,2 @@
+export interface IValoration {
+}
